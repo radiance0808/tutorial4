4160b6e73d9f7a7e090cc51b785848e5dee3ff75
radyslavburylko <burilko@icloud.com>
2020-04-03 14:51:02 +0200

    fixed

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using WebApplication1.DAL;
-using WebApplication1.Services;
+
 
 namespace WebApplication1
 {
